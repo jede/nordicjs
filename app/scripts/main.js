1946f3f683febe3edb126049d7b683cbe065ecd2
Johannes Edelstam <johannes.edelstam@gmail.com>
2014-01-23 15:32:55 +0100

    Form elements on mobile safari, disable animations on mobile, add animation for header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,24 @@
 
 
 $(function(){
+
+  if(Modernizr.touch) {
+    return;
+  }
+
   var $fadeIn = $('.fade-in-on-scroll');
   $fadeIn.each(function(i, elem){
     $(elem).addClass('fade-out');
   });
 
   $(document).scroll(function(){
-    var bottom = $(document).scrollTop() + $(window).height() * 0.85;
+    var height = $(window).height();
+    var scrollTop = $(document).scrollTop();
+    var bottom = scrollTop + height * 0.85;
+    var headerTop = $('.header-content').position().top;
+
+    $('.header-content').toggleClass('fade-out', headerTop < scrollTop - height * 0.2);
+
     $fadeIn.each(function(i, elem){
       if(bottom > $(elem).position().top) {
         $(elem).removeClass('fade-out');
